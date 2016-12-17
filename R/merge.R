f07dbc06742b844ed670d1e0d3c8ad425aa70dbe
Doug Collinge <doug.collinge@gmail.com>
2016-12-16 19:56:31 -0800

    This is a replacement for merge_, though it might be as well just to code it out, since it ends up being 1 line of R.

---
+++ R/merge.R
@@ -0,0 +1,35 @@
+# See merge_.for - this now gets the same answers, though is a function, NB.
+
+zerge = function(i,nr,ni) {
+  nn = ni
+  nn[i:nr] = ni[(i+1):(nr+1)]
+  nn
+}
+
+ni = 42 + 0:(20-1)
+i = 5
+nr = 10
+
+ni
+zerge(i,nr,ni)
+
+
+##      SUBROUTINE merge_(i,Nr,Ni)
+#      INTEGER NI(nr+2)
+#C{merge interval i and i+1}
+#c     inputs:
+#c       I- [INTEGER] interval number of the first segment to merge
+#c       NR- [INTEGER] current maximum of the interval number
+#c       NI- [INTEGER(NR)] current array with interval start point numbers
+#c
+#c     outputs:
+#c       NR- [INTEGER] new maximum of interval number
+#c       NI- [INTEGER(NR)] new array with interval start point numbers
+#      Nr=Nr-1
+#      DO J=I,Nr+1
+#          Ni(j)=Ni(j+1)
+#      END DO
+#
+#      RETURN
+#      end
+#
