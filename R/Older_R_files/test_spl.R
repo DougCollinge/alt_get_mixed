f9dd020d4c227ae9de2decbba0aebe563431117e
Doug Collinge <doug.collinge@gmail.com>
2017-01-11 16:32:04 -0800

    Made some mods to test_spl so that it now agrees with the Fortran.

--- R/Older_R_files/test_spl.R
+++ R/Older_R_files/test_spl.R
@@ -20,46 +20,60 @@
 ### Functions ###
 #################
 
-spl <- function(ni, i, nr) {
-  if( i >=nr+1 ) {"i needs to be less that nr+1"}
-  else {
-  k1=ni[i]
-  k2=ni[i+1]
-  
-  ## Feels a little silly to defining this explicitly
-  #jsplit_cond=floor((k1+k2)/2)
-  
-  ## Using double condition in an attempt to be defensive
-  #jsplit <- ifelse(jsplit_cond >= k2-1, k2-2,
-  #                 ifelse(jsplit_cond <= k1+1, k1+2, 
-  #                        "Condition Not Satisfied")
-  #)
-  
-  ## I think the following achieves the same result:
-  jsplit <- max(min(floor((k1 + k2) / 2), k2 - 2), k1 + 2) 
-  
-  nn = ni
-  nn[i:nr] = ni[(nr+1):(i+1)]
-  nn[i+1] =jsplit
-  nn
-}
+#spl <- function(ni, i, nr) {
+#  if( i >=nr+1 ) {"i needs to be less that nr+1"}
+#  else {
+#  k1=ni[i]
+#  k2=ni[i+1]
+#  
+#  ## Feels a little silly to defining this explicitly
+#  #jsplit_cond=floor((k1+k2)/2)
+#  
+#  ## Using double condition in an attempt to be defensive
+#  jsplit <- ifelse(jsplit_cond >= k2-1, k2-2,
+#                   ifelse(jsplit_cond <= k1+1, k1+2, 
+#                          "Condition Not Satisfied")
+#  )
+#  
+##  ## I think the following achieves the same result:
+##  jsplit <- max(min(floor((k1 + k2) / 2), k2 - 2), k1 + 2) 
+#  
+#  nn = ni
+#  nn[i:nr] = ni[(nr+1):(i+1)]
+#  nn[i+1] =jsplit
+#  nn
+#}
+#}
+
+spl <- function(ni,i) {
+  k1 = ni[i]
+  k2 = ni[i+1]
+  jsplit = max(min(floor((k1+k2)/2),k2-2),k1+2)
+  c(ni[1:i],jsplit,ni[(i+1):length(ni)])
 }
 
 ###############
 ### Testing ###
 ###############
 
-## Using the same data as merge.R
-ni = 42 + 0:(20-1)
-i = 5
-nr = 10
+### Using the same data as merge.R
+#ni = 42 + 0:(20-1)
+#i = 5
+#nr = 10
+#
+#spl(ni, i, nr)
+#
+#spl(ni, i=10, nr=5)
 
-spl(ni, i, nr)
-
-spl(ni, i=10, nr=5)
-
-
-
+ni = c(1,5,10,15,20)
+nr = 4
+ni
+ni = spl(ni,i=2)
+ni
+ni = spl(ni,i=5)
+ni
+ni = spl(ni,i=2)
+ni
 
 ##################################
 ### Corresponding FORTRAN CODE ###
